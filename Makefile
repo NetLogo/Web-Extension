868e896de493acad78f3ba8a908924f25006912f
TheBizzle <jason.bertsche@gmail.com>
2012-08-23 17:58:41 -0700

    Initial commit

---
+++ Makefile
@@ -0,0 +1,48 @@
+ifeq ($(origin NETLOGO), undefined)
+  NETLOGO=../..
+endif
+
+ifeq ($(origin SCALA_HOME), undefined)
+  SCALA_HOME=../..
+endif
+
+ifneq (,$(findstring CYGWIN,$(shell uname -s)))
+  COLON=\;
+  SCALA_HOME := `cygpath -up "$(SCALA_HOME)"`
+else
+  COLON=:
+endif
+
+SRCS=$(wildcard src/*.scala)
+EXT_NAME=exporter
+
+HTTP_CLIENT_NAME=httpclient-4.2
+HTTP_CLIENT_JAR=$(HTTP_CLIENT_NAME).jar
+HTTP_CLIENT_PACK=$(HTTP_CLIENT_JAR).pack.gz
+
+HTTP_CORE_NAME=httpcore-4.2
+HTTP_CORE_JAR=$(HTTP_CORE_NAME).jar
+HTTP_CORE_PACK=$(HTTP_CORE_JAR).pack.gz
+
+JAR_REPO=http://ccl.northwestern.edu/devel/
+
+$(EXT_NAME).jar $(EXT_NAME).jar.pack.gz: $(SRCS) $(HTTP_CLIENT_JAR) $(HTTP_CORE_JAR) manifest.txt Makefile
+	mkdir -p classes
+	$(SCALA_HOME)/bin/scalac -deprecation -unchecked -encoding us-ascii -classpath $(HTTP_CLIENT_JAR)$(COLON)$(HTTP_CORE_JAR)$(COLON)$(NETLOGO)/NetLogo.jar -d classes $(SRCS)
+	jar cmf manifest.txt $(EXT_NAME).jar -C classes .
+	pack200 --modification-time=latest --effort=9 --strip-debug --no-keep-file-order --unknown-attribute=strip $(EXT_NAME).jar.pack.gz $(EXT_NAME).jar
+
+$(HTTP_CLIENT_JAR) $(HTTP_CLIENT_PACK):
+	curl -f -s -S $(JAR_REPO)$(HTTP_CLIENT_JAR) -o $(HTTP_CLIENT_JAR)
+	pack200 --modification-time=latest --effort=9 --strip-debug --no-keep-file-order --unknown-attribute=strip $(HTTP_CLIENT_PACK) $(HTTP_CLIENT_JAR)
+
+$(HTTP_CORE_JAR) $(HTTP_CORE_PACK):
+	curl -f -s -S $(JAR_REPO)$(HTTP_CORE_JAR) -o $(HTTP_CORE_JAR)
+	pack200 --modification-time=latest --effort=9 --strip-debug --no-keep-file-order --unknown-attribute=strip $(HTTP_CORE_PACK) $(HTTP_CORE_JAR)
+
+$(EXT_NAME).zip: $(EXT_NAME).jar
+	rm -rf $(EXT_NAME)
+	mkdir $(EXT_NAME)
+	cp -rp $(EXT_NAME).jar $(EXT_NAME).jar.pack.gz README.md Makefile src manifest.txt $(EXT_NAME)
+	zip -rv $(EXT_NAME).zip $(EXT_NAME)
+	rm -rf $(EXT_NAME)
---
+++ src/ExporterExtension.scala
@@ -0,0 +1,136 @@
+package org.nlogo.extensions.exporter
+
+import java.io.PrintWriter
+import java.nio.charset.Charset
+
+import org.nlogo.nvm.ExtensionContext
+import org.nlogo.api.{Argument, Context, DefaultClassManager, DefaultCommand, PrimitiveManager, Syntax}
+
+class ExporterExtension extends DefaultClassManager {
+  def load(primitiveManager: PrimitiveManager) {
+    primitiveManager.addPrimitive("export", new Exporter())
+  }
+}
+
+class Exporter extends DefaultCommand with StreamHandler {
+  override def getSyntax = Syntax.commandSyntax(Array(Syntax.StringType))
+  override def getAgentClassString = "O"
+  override def perform(args: Array[Argument], context: Context) {
+    context match {
+      case extContext: ExtensionContext =>
+        val exporter = new RemoteExporter(args(0).getString) with WISEIntegration
+        exporter{(stream: java.io.OutputStream) =>
+          val writer = new PrintWriter(stream)
+          try     extContext.workspace.exportWorld(writer)
+          finally writer.close()
+        }
+      case _ => throw new IllegalArgumentException("Context is not an `ExtensionContext`!  (How did you even manage to pull that off?)")
+    }
+  }
+}
+
+private class RemoteExporter(dest: String) extends StreamHandler {
+
+  this: WebIntegration =>
+
+  import java.io.ByteArrayOutputStream
+
+  private val ExportKey = "netlogo_export"
+
+  def apply(hook: (Streamer) => Unit) {
+    val exportText = hookInForText(hook)
+    val myPostKVs  = Map(ExportKey -> Option(exportText))
+    val allPostKVs = (myPostKVs ++ kvAdditionsMap) collect { case (k, Some(v)) => (k, v) }
+    HttpHandler.httpPost(allPostKVs, dest)
+  }
+
+  protected def hookInForText(hook: (Streamer) => Unit) : String = {
+
+    val outputStream = new ByteArrayOutputStream()
+    EventEvaluator(outputStream, hook)
+
+    try     { outputStream.toString("UTF-8") }
+    catch   { case ex => System.err.println("An error has occurred in hooking/exporting: " + ex.getMessage); "" }
+    finally { outputStream.close() }
+
+  }
+
+}
+
+private[exporter] object HttpHandler {
+
+  import org.apache.http.{HttpResponse, client}, client.entity.{UrlEncodedFormEntity => URLEntity}, client.methods.HttpPost
+  import org.apache.http.message.{BasicNameValuePair => KVPair}
+
+  private val DefaultByteEncoding = "ISO-8859-1"
+  private val DestProp            = "netlogo.export_destination"
+
+  protected val httpClient = new org.apache.http.impl.client.DefaultHttpClient
+
+  def httpPost(postKVs: Map[String, String], dest: String, encoding: String = DefaultByteEncoding): String = {
+    import collection.JavaConverters.seqAsJavaListConverter
+    val destination = Option(if (!dest.isEmpty) dest else System.getProperty(DestProp)) getOrElse (throw new IllegalStateException("No valid destination given!"))
+    val post = new HttpPost(new java.net.URL(destination).toURI)
+    post.setEntity(new URLEntity((postKVs map { case (key, value) => new KVPair(key, value) } toSeq) asJava, Charset.forName("UTF-8")))
+    readResponse(httpClient.execute(post))
+  }
+
+  protected def readResponse(response: HttpResponse) = io.Source.fromInputStream(response.getEntity.getContent).mkString.trim
+
+}
+
+object EventEvaluator extends StreamHandler {
+
+  import actors.Actor
+
+  sealed protected trait TempActorProtocol
+  protected object TempActorProtocol {
+    object Start extends TempActorProtocol
+  }
+
+  import TempActorProtocol.Start
+  
+  protected class EventEvaluationActor[T](stream: T, func: (T) => Unit) extends Actor {
+    import org.nlogo.swing.Implicits.thunk2runnable
+    def act() {
+      loop {
+        react {
+          case Start => org.nlogo.awt.EventQueue.invokeLater{ () => func(stream); reply() }
+        }
+      }
+    }
+  }
+
+  def apply(stream: Streamer, hook: (Streamer) => Unit) {
+    ((new EventEvaluationActor(stream, hook)).start() !! Start)()
+  }
+
+}
+
+sealed trait StreamHandler {
+  protected type Streamer = java.io.OutputStream
+}
+
+sealed trait WebIntegration {
+  protected def kvAdditionsMap        = Map[String, Option[String]]()
+  protected def getProp(prop: String) = Option(System.getProperty(prop))
+}
+
+trait WISEIntegration extends WebIntegration {
+
+  private val PeriodIDKey    = "period_id"
+  private val RunIDKey       = "run_id"
+  private val WorkgroupIDKey = "workgroup_id"
+
+  private val PeriodIDProp    = "wise.period_id"
+  private val RunIDProp       = "wise.run_id"
+  private val WorkgroupIDProp = "wise.workgroup_id"
+
+  protected override val kvAdditionsMap = Map(
+    PeriodIDKey -> getProp(PeriodIDProp),
+    RunIDKey    -> getProp(RunIDProp),
+    WorkgroupIDKey   -> getProp(WorkgroupIDProp)
+  )
+
+}
+
