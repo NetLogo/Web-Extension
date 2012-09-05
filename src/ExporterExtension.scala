f8c62c75fcd84ea7a43a32eb9d4a1f28ce10c6c5
TheBizzle <jason.bertsche@gmail.com>
2012-09-05 16:27:10 -0500

    A slew of changes to make this extension of mine not suck so much

--- src/ExporterExtension.scala
+++ src/ExporterExtension.scala
@@ -5,6 +5,8 @@ import java.nio.charset.Charset
 
 import org.nlogo.nvm.ExtensionContext
 import org.nlogo.api.{Argument, Context, DefaultClassManager, DefaultCommand, PrimitiveManager, Syntax}
+import org.apache.http.impl.client.BasicCookieStore
+import org.apache.http.impl.cookie.BasicClientCookie
 
 class ExporterExtension extends DefaultClassManager {
   def load(primitiveManager: PrimitiveManager) {
@@ -39,13 +41,14 @@ private class RemoteExporter(dest: String) extends StreamHandler {
 
   def apply(hook: (Streamer) => Unit) {
     val exportText = hookInForText(hook)
-    val myFormat   =
-"""
+    val myFormat   = """
 {"nodeId":"node_0.jn","visitEndTime":1345775000000,"hintStates":[],"nodeStates":[{"response":"some crap"}],"visitStartTime":1345774000000,"nodeType":"JnlpNode","visitPostTime":null}
 """//.format(exportText)
     val myPostKVs  = Map(ExportKey -> Option(myFormat))
     val allPostKVs = (myPostKVs ++ kvAdditionsMap) collect { case (k, Some(v)) => (k, v) }
-    HttpHandler.httpPost(allPostKVs, dest) match { case x => println(x); x }
+    val destOpt = Option(if (!dest.isEmpty) dest else System.getProperty("netlogo.export_destination"))
+    val destination = destOpt getOrElse(throw new IllegalStateException("No valid destination given!"))
+    HttpHandler.httpPost(allPostKVs, destination, Option(System.getProperty("wise.cookie"))) match { case x => println(x); x }
   }
 
   protected def hookInForText(hook: (Streamer) => Unit) : String = {
@@ -63,23 +66,32 @@ private class RemoteExporter(dest: String) extends StreamHandler {
 
 private[exporter] object HttpHandler {
 
-  import org.apache.http.{HttpResponse, client}, client.entity.{UrlEncodedFormEntity => URLEntity}, client.methods.HttpPost
-  import org.apache.http.message.{BasicNameValuePair => KVPair}
+  import org.apache.http.{ HttpResponse, client, message }
+  import message.{ BasicNameValuePair => KVPair }
+  import client.entity.{ UrlEncodedFormEntity => URLEntity }, client.methods.HttpPost
 
-  private val DefaultByteEncoding = "ISO-8859-1"
-  private val DestProp            = "netlogo.export_destination"
+  private val DefaultByteEncoding = "UTF-8"
 
   protected def httpClient = new org.apache.http.impl.client.DefaultHttpClient
 
-  def httpPost(postKVs: Map[String, String], dest: String, encoding: String = DefaultByteEncoding): String = {
+  def httpPost(postKVs: Map[String, String], dest: String,
+               cookieValue: Option[String] = None, encoding: String = DefaultByteEncoding): String = {
+
     import collection.JavaConverters.seqAsJavaListConverter
-    val destination = Option(if (!dest.isEmpty) dest else System.getProperty(DestProp)) getOrElse (throw new IllegalStateException("No valid destination given!"))
-    val post = new HttpPost(new java.net.URL(destination).toURI)
-    post.setEntity(new URLEntity((postKVs map { case (key, value) => new KVPair(key, value) } toSeq) asJava, Charset.forName("UTF-8")))
+
+    val post = new HttpPost(new java.net.URL(dest).toURI)
+    val javaKVs = (postKVs map { case (key, value) => new KVPair(key, value) } toSeq).asJava
+    post.setEntity(new URLEntity(javaKVs, Charset.forName(encoding)))
+
+    // Many, many "official" cookie-insertion approaches were tried; all failed --JAB (9/5/12)
+    cookieValue foreach (cookie => post.setHeader("COOKIE", "JSESSIONID=" + cookie))
+
     readResponse(httpClient.execute(post))
+
   }
 
-  protected def readResponse(response: HttpResponse) = response.getStatusLine + ":\n" + io.Source.fromInputStream(response.getEntity.getContent).mkString.trim
+  protected def readResponse(response: HttpResponse) =
+    response.getStatusLine + ":\n" + io.Source.fromInputStream(response.getEntity.getContent).mkString.trim
 
 }
 
@@ -124,16 +136,16 @@ trait WISEIntegration extends WebIntegration {
 
   private val PeriodIDKey    = "periodId"
   private val RunIDKey       = "runId"
-  private val WorkgroupIDKey = "workgroupId"
+  private val WorkgroupIDKey = "userId"
 
   private val PeriodIDProp    = "wise.period_id"
   private val RunIDProp       = "wise.run_id"
   private val WorkgroupIDProp = "wise.workgroup_id"
 
   protected override val kvAdditionsMap = Map(
-    PeriodIDKey -> getProp(PeriodIDProp),
-    RunIDKey    -> getProp(RunIDProp),
-    WorkgroupIDKey   -> getProp(WorkgroupIDProp)
+    PeriodIDKey    -> getProp(PeriodIDProp),
+    RunIDKey       -> getProp(RunIDProp),
+    WorkgroupIDKey -> getProp(WorkgroupIDProp)
   )
 
 }
