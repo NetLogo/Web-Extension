74048b38c15222c131b10da90bf358cfa473afd7
TheBizzle <jason.bertsche@gmail.com>
2012-10-19 13:31:50 -0500

    Removing todos

--- src/WebExtension.scala
+++ src/WebExtension.scala
@@ -4,9 +4,6 @@ import org.nlogo.api.{ DefaultClassManager, PrimitiveManager }
 
 import prim._
 
-//@ Add ability to read a file by passed-in Java property
-//@ Add ability to directly read file from a stringed URL
-//@ Should be able to set property to determine which `WebIntegration` to use for the whole life of the run
 class WebExtension extends DefaultClassManager {
   def load(primitiveManager: PrimitiveManager) {
     primitiveManager.addPrimitive("export-world", new ExportWorld())
