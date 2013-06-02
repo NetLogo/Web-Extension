cba0ca3afa259ea8b5a27281fc747e38ef69f11c
Corey Brady <cbrady@inquirelearning.com>
2013-06-01 23:09:54 -0400

    more pleasing layout (cherry picked from commit a1f6a4a73b20e8a359ec7c046944c97cb8b7a447)

--- src/prim/ImportModel.scala
+++ src/prim/ImportModel.scala
@@ -26,21 +26,22 @@ import
 object ImportModel extends WebCommand with SimpleWebPrimitive {
 
   override def perform(args: Array[Argument])(implicit context: Context, ignore: DummyImplicit) {
-    ensuringExtensionContext { (extContext: ExtensionContext) =>
-      val (dest)         = processArguments(args)
-      val modelNameRegex = """[^/]+$""".r
-      val modelNameMatch = modelNameRegex.findFirstMatchIn( dest ).getOrElse( throw new ExtensionException("Malformed Model URL: " + dest) )
-      val modelName      = modelNameMatch.matched
-      val tempDirName    = System.getProperty("java.io.tmpdir")
-      using(new BufferedInputStream( new URL(dest).openStream() )) {
-        closeable =>
-          val destFile = new java.io.File(tempDirName + modelName)
-          if (destFile.exists()) destFile.delete()
-          FileWriter(closeable, tempDirName, modelName)
-      }
-      invokeLater{ {
-        org.nlogo.app.App.app.fileMenu.openFromPath(tempDirName + modelName, ModelType.Normal)
-      } }
+    ensuringExtensionContext {
+      extContext =>
+        val (dest)         = processArguments(args)
+        val modelNameRegex = """[^/]+$""".r
+        val modelNameMatch = modelNameRegex.findFirstMatchIn( dest ).getOrElse( throw new ExtensionException("Malformed Model URL: " + dest) )
+        val modelName      = modelNameMatch.matched
+        val tempDirName    = System.getProperty("java.io.tmpdir")
+        using(new BufferedInputStream( new URL(dest).openStream() )) {
+          closeable =>
+            val destFile = new java.io.File(tempDirName + modelName)
+            if (destFile.exists()) destFile.delete()
+            FileWriter(closeable, tempDirName, modelName)
+        }
+        invokeLater{ {
+          org.nlogo.app.App.app.fileMenu.openFromPath(tempDirName + modelName, ModelType.Normal)
+        } }
     }
 
   }
