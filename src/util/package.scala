dd5086187994ef8e4ab1bcb20e910d4deb34a384
TheBizzle <jason.bertsche@gmail.com>
2013-06-03 13:14:03 -0500

    Adding a method to 'util' for running on event thread (cherry picked from commit 3a030b1b874135d4bcf57a021e0ba2f33398e1ca)

--- src/util/package.scala
+++ src/util/package.scala
@@ -1,5 +1,8 @@
 package org.nlogo.extensions.web
 
+import
+  javax.swing.SwingUtilities
+
 /**
  * Created with IntelliJ IDEA.
  * User: jason
@@ -8,7 +11,16 @@ package org.nlogo.extensions.web
  */
 
 package object util {
+
   type Streamer = java.io.OutputStream
+
   def using[A <: { def close() }, B](stream: A)(f: A => B) : B =
     try { f(stream) } finally { stream.close() }
+
+  def doLater(body: => Unit) {
+    SwingUtilities.invokeLater(new Runnable() {
+      override def run() { body }
+    })
+  }
+
 }
