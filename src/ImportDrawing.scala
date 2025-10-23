fe0a3998301051dd31285597ed01e35e9e29c001
ERSUCC <isaacbrown5763@gmail.com>
2025-10-22 20:07:47 -0500

    Use better method of checking for headless mode

--- src/ImportDrawing.scala
+++ src/ImportDrawing.scala
@@ -5,17 +5,16 @@ import java.net.URL
 import org.nlogo.api.{ Argument, Command, Context, ExtensionException, Workspace }
 import org.nlogo.core.Syntax.{ commandSyntax, ListType, StringType }
 import org.nlogo.nvm.ExtensionContext
-import org.nlogo.window.GUIWorkspace
 
 object ImportDrawing extends WebPrimitive with Command {
 
   override def getSyntax = commandSyntax(List(StringType))
 
   override def perform(args: Array[Argument], context: Context): Unit = carefully {
-    EnsuranceAgent.ensuringGUIWorkspace(context.workspace) { (guiWS: GUIWorkspace) =>
+    if (context.workspace.workspaceContext.workspaceGUI) {
       val dest = args(0).getString
       using(new URL(dest).openStream()) {
-        guiWS.importDrawing(_)
+        context.workspace.importDrawing(_)
       }
     }
   }
@@ -27,26 +26,14 @@ object ImportDrawingFine extends WebPrimitive with Command {
   override def getSyntax = commandSyntax(List(StringType, StringType, ListType))
 
   override def perform(args: Array[Argument], context: Context): Unit = carefully {
-    EnsuranceAgent.ensuringGUIWorkspace(context.workspace) { (guiWS: GUIWorkspace) =>
+    if (context.workspace.workspaceContext.workspaceGUI) {
       val dest      = args(0).getString
       val reqMethod = httpMethodify(args(1)).getOrElse(throw new ExtensionException("Invalid HTTP method name supplied."))
       val paramMap  = paramify     (args(2)).getOrElse(Map.empty)
       processResponse(mkRequest(dest, reqMethod, paramMap, Map.empty)) {
-        case (response, _) => guiWS.importDrawing(response)
+        case (response, _) => context.workspace.importDrawing(response)
       }
     }
   }
 
-}
-
-private object EnsuranceAgent {
-  def ensuringGUIWorkspace[T](ws: Workspace)(f: (GUIWorkspace) => T): T = {
-    ws match {
-      case guiWS: GUIWorkspace =>
-        f(guiWS)
-      case other =>
-        val message = s"Cannot use this primitive from any type of workspace by a `GUIWorkspace`; you're using a ${other.getClass.getName}."
-        throw new UnsupportedOperationException(message)
-    }
-  }
 }
